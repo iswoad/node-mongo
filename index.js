58f1de35a086ab1ad6b37f450b8f433c82d37783
iswoad <iswoadansary@gmail.com>
2021-06-14 13:47:14 +0600

    mongo learn first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const epxress = require('express');
+
+
+const app = epxress();
+
+
+app.get('/', (req, res) => {
+    res.send('Thank you for calling nigger')
+  })
+
+
+app.listen(3000, () => console.log('listening to port 3000'))
